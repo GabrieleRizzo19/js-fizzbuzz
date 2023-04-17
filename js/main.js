b2b31fb66fe1adf57a3537df30558c05ea344433
GabrieleRizzo19 <gabryrizz02@gmail.com>
2023-04-17 18:25:34 +0200

    esercizio finito

--- js/main.js
+++ js/main.js
@@ -1,21 +1,25 @@
 const container = document.getElementById("box-container");
 
-for (i = 1 ; i <= 15 ; i++ ){
+for (i = 1 ; i <= 100 ; i++ ){
     
     let numero = i;
+    let coloreBackground;
     
-    if((numero % 3) == 0){
-        numero = "Fizz";
-    }
-
-    if((numero % 5) == 0){
-        numero += "Buzz";
-    }
-
     if(((numero % 3) == 0) && ((numero % 5) == 0)){
         numero = "FizzBuzz";
+        coloreBackground = "bg-danger";
+    }else if((numero % 5) == 0){
+        numero = "Buzz";
+        coloreBackground = "bg-warning";
+    }else if((numero % 3) == 0){
+        numero = "Fizz";
+        coloreBackground = "bg-success";
     }
 
     console.log(numero);
 
+    const NuovaBox = document.createElement("div");
+    NuovaBox.append(numero);
+    NuovaBox.className = `box ${coloreBackground}`;
+    container.append(NuovaBox);
 }
